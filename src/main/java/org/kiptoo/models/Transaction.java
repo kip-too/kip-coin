4563e6bd4111ca20878205e7a63dd751af4c9a39
kip-too <hoseakiptoo201@gmail.com>
2023-04-17 15:32:43 +0300

    transaction

--- src/main/java/org/kiptoo/models/Transaction.java
+++ src/main/java/org/kiptoo/models/Transaction.java
@@ -1,4 +1,35 @@
 package org.kiptoo.models;
+import java.io.Serializable;
+import java.security.InvalidKeyException;
+import java.security.Signature;
+import java.security.SignatureException;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Base64;
 
-public class Transaction {
+
+public class Transaction implements  Serializable{
+   private  byte[] from;
+   private String fromFX;
+   private byte[] to;
+   private String toFX;
+   private Integer value;
+   private  String timestamp;
+   private byte[] signature;
+   private String signatureFX;
+   private Integer ledgerId;
+
+    public Transaction(byte[] from, String fromFX, byte[] to, String toFX, Integer value, String timestamp, byte[] signature, String signatureFX, Integer ledgerId) {
+        Base64.Encoder encoder = Base64.getEncoder();
+        this.from = from;
+        this.fromFX = fromFX;
+        this.to = to;
+        this.toFX = toFX;
+        this.value = value;
+        this.timestamp = timestamp;
+        this.signature = signature;
+        this.signatureFX = signatureFX;
+        this.ledgerId = ledgerId;
+    }
 }
+
